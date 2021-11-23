41d12b30c80bf9f0f2eb01989f2a7d4721ab2f4e
JelissaReyes <91210594+JelissaReyes@users.noreply.github.com>
2021-11-22 23:40:10 -0500

    Queue assignment for CS 10403

---
+++ PythonQueue.py
@@ -0,0 +1,31 @@
+#Queue at a bank
+
+
+names = []
+
+
+names.append('sally')
+names.append('joe')
+names.append('kevin')
+names.append('sam')
+names.append('ryan')
+names.append('kelly')
+names.append('matt')
+names.append('jil')
+names.append('john')
+names.append('jelissa')
+
+
+len(names)
+
+
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
+print(names.pop(0))
